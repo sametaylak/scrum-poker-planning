2879411c759fe7079749736f0ba307333c46f1ad
Samet Aylak <sametaylak29@gmail.com>
2019-10-06 20:42:27 +0300

    styles moved to seperate file

--- client/src/App.vue
+++ client/src/App.vue
@@ -17,45 +17,5 @@ export default {
 </script>
 
 <style lang="scss">
-html, body {
-  margin: 0;
-  padding: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-}
-
-.container {
-  margin-right: auto;
-  margin-left: auto;
-  max-width: 960px;
-  padding-right: 10px;
-  padding-left: 10px;
-}
-
-.flex {
-  display: flex;
-}
-
-.flex-primary {
-  flex: 1;
-}
-
-.btn {
-  display: block;
-  background: none;
-  border: none;
-  padding: 8px 16px;
-  cursor: pointer;
-  &.primary {
-    background: $primary;
-    color: $white;
-  }
-  &:disabled {
-    cursor: not-allowed;
-    background: $gray;
-    color: black;
-  }
-}
+@import '@/styles/app.scss'
 </style>
